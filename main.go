78973cf1d2421edb2b298ec099a0d3ca5b2d4af1
sihaixianyu <sihaixianyu@qq.com>
2023-02-14 16:09:27 +0800

    2023-02-14

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	if err := r.Run("127.0.0.1:8888"); err != nil {
+		panic(fmt.Sprintf("[Main] err: %s", err))
+	}
+}
